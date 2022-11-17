fc6065917aad950a58d1232925bba5a0b12497b4
Brandon S Sample <brandonsample447@gmail.com>
2022-11-17 13:57:58 -0500

    update 2.3
    minor changes to menu, completed rounding of results

--- converter_test_side.py
+++ converter_test_side.py
@@ -1,5 +1,7 @@
 
 print("\nWelcome to Brandon and Logan's Unit Converter Program\n") 
+print("Below is a list of all possible conversions, please enter your request as")
+print(":convert (thing 1) to (thing 2)")
 
 
 conversions_available = [('°F', '°C'),
@@ -86,120 +88,90 @@ while(True):
     except: print("Invalid Input, Please use syntax like 'convert 32 f to c'" )
 
     
-   
-
     
     if conversion == 1:
-        to_value = (from_value - 32) / 1.8
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}') 
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value-32 / 1.8, to_unit))
 
     elif conversion == 2:
-        to_value = from_value * 1.8 + 32
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value *1.8 + 32, to_unit))
 
     elif conversion == 3:
-        to_value = from_value * 0.45
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value* 0.45, to_unit))
 
     elif conversion == 4:
-        to_value = from_value * 2.22
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value*2.22, to_unit))
 
     elif conversion == 5:
-        to_value = from_value * 8
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}') 
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value* 8, to_unit))
 
     elif conversion == 6:
-        to_value = from_value / 8
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 8, to_unit))
 
     elif conversion == 7:
-        to_value = (from_value *16)
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value* 16, to_unit))
 
     elif conversion == 8:
-        to_value = (from_value /16)
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 8, to_unit))
 
     elif conversion == 9:
-        to_value = (from_value /2)
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 2, to_unit))
 
     elif conversion == 10:
-        to_value = (from_value *2)
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')    
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value* 2, to_unit)) 
 
     elif conversion == 11:
-        to_value = from_value * 48
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value* 48, to_unit))
 
     elif conversion == 12:
-        to_value = from_value / 48
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 48, to_unit))
 
     elif conversion == 13:
-        to_value = from_value / 4
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/4, to_unit))
 
     elif conversion == 14:
-        to_value = from_value * 4
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value* 4, to_unit))
 
     elif conversion == 15:
-        to_value = from_value / 16
-        print(f'{from_value} {from_unit}  ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 16, to_unit))
 
     elif conversion == 16:
-        to_value = from_value * 16
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value *16, to_unit))
 
     elif conversion == 17:
-        to_value = from_value * 3
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value *3, to_unit))
 
     elif conversion == 18:
-        to_value = from_value / 3
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value /3, to_unit))
 
     elif conversion == 19:
-        to_value = from_value / 2
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 2, to_unit))
 
     elif conversion == 20:
-        to_value = from_value * 2
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value * 2, to_unit))
 
     elif conversion == 21:
-        to_value = from_value / 16
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 16, to_unit))
 
     elif conversion == 22:
-        to_value = from_value * 16
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value* 16, to_unit))
 
     elif conversion == 23:
-        to_value = from_value / 14.787
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 14.787, to_unit))
 
     elif conversion == 24:
-        to_value = from_value * 14.787
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+       print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value * 14.787, to_unit))
 
     elif conversion == 25:
-        to_value = from_value * 29.574
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value* 29.574, to_unit))
 
     elif conversion == 26:
-        to_value = from_value / 29.574
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 29.574, to_unit))
 
     elif conversion == 27:
-        to_value = from_value * 240
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value*240, to_unit))
 
     elif conversion == 28:
-        to_value = from_value / 240
-        print(f'{from_value} {from_unit} ( Is Equal to ) -> {to_value} {to_unit}')
+      print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value/ 240, to_unit))
 
     elif conversion == 29:
       print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value*4.167, to_unit))
@@ -248,3 +220,5 @@ while(True):
 
     elif conversion == 44:
         print('{} {} ( Is Equal to ) -> {:.2f} {}'.format(from_value, from_unit, from_value*2.205, to_unit))
+
+  
